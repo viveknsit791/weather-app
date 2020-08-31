9425425eec9dbed26d8b24cb30dc2d3e42c24996
Vivek Rajput <vivek.rajput@paytm.com>
2020-08-31 22:30:37 +0530

    udpated

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,35 +1,25 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
+const http = require("http");
+// console.log(process.argv);
+// const address = process.argv[2];
 
-console.log(process.argv);
-const address = process.argv[2];
-
-if(address){
-    geocode(address, (error, { lat, long } = {}) => {
-        if(error){
-            return console.log(error);
-        }
-        forecast(lat, long, (error, { temperature, feelslike } = {}) => {
-            if(error){
-                return console.log("Error");
-            }
-            console.log("Current Temperature : " + temperature + "\nFeels Like Temperature : " + feelslike);
-        });    
-    })
-}
-else {
-    console.log("Please enter some city to find the temperature");
-}
-
-
-
-
-
-
-
-
-
-
+// if(address){
+//     geocode(address, (error, { lat, long } = {}) => {
+//         if(error){
+//             return console.log(error);
+//         }
+//         forecast(lat, long, (error, { temperature, feelslike } = {}) => {
+//             if(error){
+//                 return console.log("Error");
+//             }
+//             console.log("Current Temperature : " + temperature + "\nFeels Like Temperature : " + feelslike);
+//         });    
+//     })
+// }
+// else {
+//     console.log("Please enter some city to find the temperature");
+// }
 
 
 
@@ -68,3 +58,26 @@ else {
 // add(1, 4, (sum) => {
 //     console.log(sum) // Should print: 5
 // })
+const url = `http://api.weatherstack.com/current?access_key=823a3d3cc016b4d0eb58807fd52a8c12&query=40,-75`
+const request = http.request(url, (response) => {
+    let data = '';
+    
+    response.on('data', (chunk) => {
+        data = data + chunk.toString();
+    });
+
+    response.on('end', () => {
+        const body = JSON.parse(data);
+        console.log(body);
+    });
+});
+
+
+request.on('error',(error) => {
+    console.log(error);
+})
+
+request.on('end');
+
+
+
